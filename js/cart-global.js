d887d9eced81fb033d2cde3a2911f97b34d35d70
india-PC\india <santosh.patnala@gmail.com>
2018-04-19 18:17:37 +0530

    cart modifications

--- js/cart-global.js
+++ js/cart-global.js
@@ -60,47 +60,47 @@
     //REVIEW TOOLTIP START
     //$('.pdp_star-ratings').popover();
     var popoverTemplate = ['<div class="timePickerWrapper popover">',
-                                '<div class="arrow"></div>',
-                                '<div class="popover-content">',
-                                '</div>',
-                            '</div>'].join('');
+    '<div class="arrow"></div>',
+    '<div class="popover-content">',
+    '</div>',
+    '</div>'].join('');
 
     var content = ['<ul class="pdp_progress-bar-wrapper">',
-        '<li>',
-        '<span><span class="star-count">5</span>★</span>',
-        '<div class="progress">',
-        '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"style="width:90%">9540 Customers</div>',
-        '</div>',
-        '</li>',
-        '<li>',
-        '<span><span class="star-count">4</span>★</span>',
-        '<div class="progress">',
-        '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"style="width:70%">954 Customers</div>',
-        '</div>',
-        '</li>',
-        '<li>',
-        '<span><span class="star-count">3</span>★</span>',
-        '<div class="progress">',
-        '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"style="width:50%">95 Customers</div>',
-        '</div>',
-        '</li>',
-        '<li>',
-        '<span><span class="star-count">2</span>★</span>',
-        '<div class="progress">',
-        '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"style="width:40%">9 Customers</div>',
-        '</div>',
-        '</li>',
-        '<li>',
-        '<span><span class="star-count">1</span>★</span>',
-        '<div class="progress">',
-        '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"style="width:30%">1 Customer</div>',
-        '</div>',
-        '</li>',
-        '<li><a href="#customerReviews" class="pdp_see-review-btn">See All Reviews</a></li>',
-        '</ul>', ].join('');
+    '<li>',
+    '<span><span class="star-count">5</span>★</span>',
+    '<div class="progress">',
+    '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"style="width:90%">9540 Customers</div>',
+    '</div>',
+    '</li>',
+    '<li>',
+    '<span><span class="star-count">4</span>★</span>',
+    '<div class="progress">',
+    '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"style="width:70%">954 Customers</div>',
+    '</div>',
+    '</li>',
+    '<li>',
+    '<span><span class="star-count">3</span>★</span>',
+    '<div class="progress">',
+    '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"style="width:50%">95 Customers</div>',
+    '</div>',
+    '</li>',
+    '<li>',
+    '<span><span class="star-count">2</span>★</span>',
+    '<div class="progress">',
+    '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"style="width:40%">9 Customers</div>',
+    '</div>',
+    '</li>',
+    '<li>',
+    '<span><span class="star-count">1</span>★</span>',
+    '<div class="progress">',
+    '<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"style="width:30%">1 Customer</div>',
+    '</div>',
+    '</li>',
+    '<li><a href="#customerReviews" class="pdp_see-review-btn">See All Reviews</a></li>',
+    '</ul>', ].join('');
 
     $('body').popover({
-        selector: '.pdp_star-ratings',
+        selector: '.pdp_star-popup',
         trigger: 'click',
         content: content,
         template: popoverTemplate,
@@ -108,7 +108,7 @@
         html: true
     });
     //REVIEW TOOLTIP END
-	
+
     //REVIEW POPOVER BTN SCROLL START
     $("body").on("click", "a.pdp_see-review-btn", function () {
         var linkHref = $(document).find("a.pdp_see-review-btn").attr("href");
@@ -121,11 +121,11 @@
     //REVIEW POPOVER BTN SCROLL END
 
     //ZOOM IN IMAGE START
-	zoomImg();
+    zoomImg();
     $('.pdp_thumb-img img').click(function () {
         $('.pdp_zoom-image').attr('src', $(this).attr('src'));
-		$('.zoomContainer').remove();
-		$('.pdp_zoom-image').removeData('elevateZoom');
+        $('.zoomContainer').remove();
+        $('.pdp_zoom-image').removeData('elevateZoom');
         zoomImg();
     });
 	//ZOOM IN IMAGE END
@@ -141,35 +141,121 @@
 	});
 	
 	$('.flexslider').flexslider({
-    animation: "slide",
-    animationLoop: true,
-    itemWidth: 210,
-    itemMargin: 5,
-    mousewheel: true,
-	minItems: 2,
-    maxItems: 4,
-	move: 1,
-    slideshow: 1,
-    animationSpeed: 1500,
-    slideshowSpeed: 3000,
-    rtl: true,
-	randomize: false,
+        animation: "slide",
+        animationLoop: true,
+        itemWidth: 210,
+        itemMargin: 5,
+        mousewheel: true,
+        minItems: 2,
+        maxItems: 4,
+        move: 1,
+        slideshow: 1,
+        animationSpeed: 1500,
+        slideshowSpeed: 3000,
+        rtl: true,
+        randomize: false,
         pauseOnHover: true
-  });
+    });
     //SHOPPING CART SCRIPT END
+    
+    ////USED TO HIDE THE POPOVER, SHARE AND COMPARE POPOVERS CLOSE ON DOC. CLICK START
+    /*$(document).click(function(e) 
+    {
+        var shareContainer = $(".pdp_share-tab");
+        var compareContainer = $(".pdp_compare-tab");
+        var shareClickBtn=$(".pdp-share-btn");
+        var compareClickBtn=$(".pdp-compare-btn");
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!shareContainer.is(e.target) && !compareContainer.is(e.target) && !shareClickBtn.is(e.target) && !compareClickBtn.is(e.target) && shareContainer.has(e.target).length === 0 && compareContainer.has(e.target).length === 0)
+        {
+            shareContainer.hide();
+            compareContainer.hide();
+            shareClickBtn.css('border-color', '#ccc');
+            compareClickBtn.css('border-color', '#ccc');
+        }
+    });*/
+    $(document).on('click', function(e) {
+        if ( $(e.target).closest('.pdp-share-btn,.pdp-compare-btn').length ) {
+            $(".pdp_share-tab, .pdp_compare-tab").show();
+        }else if ( ! $(e.target).closest('.pdp_share-tab, .pdp_compare-tab').length ) {
+            $('.pdp_share-tab, .pdp_compare-tab').hide();
+        }
+    });
+    ////USED TO HIDE THE POPOVER, SHARE AND COMPARE POPOVERS CLOSE ON DOC. CLICK END
+
+    //PAGINATION START
+    $('#pagination-demo').twbsPagination({
+totalPages: 3,
+// the current page that show on start
+startPage: 1,
+
+// maximum visible pages
+visiblePages: 5,
+
+initiateStartPageClick: true,
+
+// template for pagination links
+href: false,
+
+// variable name in href template for page number
+hrefVariable: '{{number}}',
+
+// Text labels
+first: 'First',
+prev: 'Previous',
+next: 'Next',
+last: 'Last',
+
+// carousel-style pagination
+loop: false,
+
+// callback function
+onPageClick: function (event, page) {
+    $('.page-active').removeClass('page-active');
+  $('#page'+page).addClass('page-active');
+},
+
+// pagination Classes
+paginationClass: 'pagination',
+nextClass: 'next',
+prevClass: 'prev',
+lastClass: 'last',
+firstClass: 'first',
+pageClass: 'page',
+activeClass: 'active',
+disabledClass: 'disabled'
+
+});
+    //PAGINATION END
+    //UP DOWN VOTE VALUE CHANGE START
+    $(".pdp_review-up-down-vote-container").find("a.pdp_up-vote").on("click",function(){
+        var voteVal=parseInt($(this).next("span").text());
+        temp=voteVal;
+        if(voteVal==temp){
+
+            voteVal=voteVal+1;
+            $(this).next("span").text(voteVal);
+            return false;
+        }
+        if(voteVal!==temp){
+            voteVal=voteVal-1;
+            console.log("if 02"+voteVal);
+        }
+    });
+    //UP DOWN VOTE VALUE CHANGE END
 });
 function zoomImg(){
 	$(".pdp_zoom-image").elevateZoom({
         // zoomType: "inner",
         // cursor: "crosshair"
-		
-		gallery: 'pdp_thumb-img',
-lensSize: 400,
-cursor: 'pointer',
-galleryActiveClass: 'active',
-imageCrossfade: true,
-scrollZoom : true,
-responsive: true
+
+        gallery: 'pdp_thumb-img',
+        lensSize: 400,
+        cursor: 'pointer',
+        galleryActiveClass: 'active',
+        imageCrossfade: true,
+        scrollZoom : true,
+        responsive: true
     });
 }
 
